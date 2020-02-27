0a2d5ce4150579df07dcae36774cd55bdd999b9a
ryunosinfx <ryunosinfxxul@gmail.com>
2020-02-28 01:23:23 +0900

    Queueを設ける

--- src/webmessaging/YadorigiMessenger.js
+++ src/webmessaging/YadorigiMessenger.js
@@ -1,6 +1,9 @@
 import { MessageChannel } from '../view/util/MessagingChannel';
 import { Cryptor } from '../util/Cryptor';
 import { Hasher } from '../util/Hasher';
+import { YadrogiLoginManager } from './YadrogiLoginManager';
+const TIMEOUT_MAX = 60 * 1000;
+const taskMap = {};
 export class YadorigiMessenger {
 	constructor(url = 'dummy', group, userId, passphrase, onMessageCallback = async () => {}) {
 		this.url = url;
@@ -13,11 +16,31 @@ export class YadorigiMessenger {
 	static getInstance(url, group, userId, passphrase, onMessageCallback) {
 		return new YadorigiMessenger(url, group, userId, passphrase, onMessageCallback);
 	}
+	async init() {
+		this.key = null;
+		const loginKey = JSON.stringify([this.group, this.userId]);
+		const keyData = await YadrogiLoginManager.login(loginKey, this.passphrase);
+		this.key = await Cryptor.importKeyAESGCM(keyData);
+	}
 	async post(msg) {
-		const encrypted = await Cryptor.encodeStrAES256GCM(msg, this.passphrase);
-		const hash = await Hasher.sha512(encrypted);
-		this.ms.send();
-		return;
+		const current = Date.now();
+		const encrypted = await Cryptor.encodeStrAES256GCM(msg, this.key);
+		const hash = await Hasher.sha512(encrypted + '/' + current);
+		this.ms.send({ encrypted, hash, current });
+		return this.addQueue(hash);
+	}
+	addQueue(hash) {
+		return new Promise(resolve => {
+			const task = data => {
+				resolve(data);
+				delete taskMap[hash];
+			};
+			taskMap[hash] = task;
+			setTimeout(() => {
+				resolve('timeout');
+				delete taskMap[hash];
+			}, TIMEOUT_MAX);
+		});
 	}
 	getCallBack() {
 		return data => {
@@ -25,12 +48,30 @@ export class YadorigiMessenger {
 		};
 	}
 	async onMessage(data) {
-		if (this.isValid(data)) {
-			this.onMessageCallback(data);
-			const result = await this.onMessageCallback();
+		const decoded = this.decode(data);
+		if (decoded) {
+			const result = await this.onMessageCallback(decoded);
 			this.post(result);
 		}
 	}
-	isValid(msg) {}
+	async decode(msg) {
+		const current = Date.noe();
+		try {
+			let data = typeof msg === 'string' ? JSON.parse(msg) : msg;
+			if (!data || !data.encrypted || !data.hash || isNaN((data.current + '') * 1) || data.current - current > 1000) {
+				return false;
+			}
+			const hash = await Hasher.sha512(encrypted + '/' + current);
+			if (hash !== data.hash) {
+				return false;
+			}
+
+			const decoded = await Cryptor.decodeAES256GCMasStr(data.encrypted, this.key);
+			return decoded;
+		} catch (e) {
+			console.warn(e);
+			return false;
+		}
+	}
 	validation() {}
 }
