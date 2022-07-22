6929a7d5f8bb96f517eda3704399451a2133a7f6
ryunosinfx <ryunosinfxxul@gmail.com>
2022-07-23 06:22:52 +0900

    現状のビルドセッティング

--- webpack.config.js
+++ webpack.config.js
@@ -7,21 +7,23 @@ module.exports = {
 	entry: {
 		bundle: './src/main.js',
 		worker: './src/worker.js',
-		tests: './src/gs/tests.js',
+		// tests: './src/gs/tests.js',
 
-		test: ['./test/util/Cryptor.test.js', './test/util/Deflater.test.js', './test/webrtc/YadorigiFileProsessor.test.js', './test/webrtc/YadorigiSignalingAdupter.test.js'],
+		test: ['./test/util/Cryptor.test.js', './test/util/Deflater.test.js'], // './test/webrtc/YadorigiFileProsessor.test.js', './test/webrtc/YadorigiSignalingAdupter.test.js'],
 	},
 	// [
 	//   './src/main.js','./src/worker.js', './index.css'
 	// ],mymodernmet.com/studio-ghibli-virtual-backgrounds/
 	output: {
 		// 出力するファイル名
+		// filename: '[name].js',
 		filename: '[name].js',
 		// 出力先のパス
-		path: __dirname + '/dist',
+		// path: __dirname + '/public',
+		path: path.join(__dirname, 'public'),
 		//publicPath: __dirname + "/dest/js",
 		webassemblyModuleFilename: '[modulehash].wasm',
-		publicPath: './',
+		publicPath: '/',
 		globalObject: 'this',
 	},
 	module: {
@@ -36,7 +38,7 @@ module.exports = {
 			},
 			{
 				test: /\.js$/,
-				exclude: /node_modules/,
+				exclude: [/node_modules/, '/src/gs/'],
 			},
 			{
 				test: /\.wasm$/,
@@ -44,13 +46,13 @@ module.exports = {
 			},
 			{
 				test: /test\.js$/,
-				exclude: /node_modules/,
+				exclude: [/node_modules/, '/src/gs/'],
 			},
 		],
 	},
 	devServer: {
 		port: 8085,
-		hot: true,
+		hot: 'only',
 		open: ['/index.html'],
 		watchFiles: {
 			paths: ['src/**/*.js', 'dist/**/*'],
@@ -58,10 +60,23 @@ module.exports = {
 				usePolling: false,
 			},
 		},
-		static: {
-			directory: path.join(__dirname, 'dist'),
-			serveIndex: true,
-			watch: false,
+		// contentBase: path.join(__dirname, 'dist'),
+		// publicPath: '/',
+		// static: {
+		// 	directory: path.join(__dirname, 'dist'),
+		// 	serveIndex: true,
+		// 	watch: false,
+		// },
+		client: {
+			logging: 'info',
+			// Can be used only for `errors`/`warnings`
+			//
+			// overlay: {
+			//   errors: true,
+			//   warnings: true,
+			// }
+			overlay: true,
+			progress: true,
 		},
 	},
 	plugins: [
