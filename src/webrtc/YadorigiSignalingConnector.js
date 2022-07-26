7eea4d8021f9c16ab3b0d95aa348589e275e68fd
ryunosinfx <ryunosinfxxul@gmail.com>
2022-07-27 01:13:11 +0900

    フォーマット習性

--- src/webrtc/YadorigiSignalingConnector.js
+++ src/webrtc/YadorigiSignalingConnector.js
@@ -14,35 +14,26 @@ export class YadorigiSignalingConnector {
 	 * @param {*} payload
 	 */
 	async putSdp(groupNameHash, fileName, hash, payload) {
-		this.l.log(`--putSdp--0----------YadorigiSignalingConnector--------------------------------------groupNameHash:${groupNameHash}`);
-		const data = { group: groupNameHash, fileName, hash, data: payload };
-		this.l.log(`--putSdp--1----------YadorigiSignalingConnector--------------------------------------fileName:${fileName}`);
-		await this.Fetcher.postAsSubmit(this.endPoint, data);
-		this.l.log(`--putSdp--2----------YadorigiSignalingConnector--------------------------------------hash:${hash}`);
-		this.l.warn(`--putSdp--3----------YadorigiSignalingConnector--------------------------------------payload:${payload}`);
+		this.l.log(`--putSdp--1----------YadorigiSignalingConnector-PUT-SPD-START------------------------groupNameHash:${groupNameHash}/fileName:${fileName}`);
+		await this.Fetcher.postAsSubmit(this.endPoint, { group: groupNameHash, fileName, hash, data: payload });
+		this.l.warn(`--putSdp--2----------YadorigiSignalingConnector-PUT-SPD-DONE------------------------hash:${hash}/payload:${payload}`);
 	}
 	async getSdp(groupNameHash, fileName) {
-		this.l.log(`--getSdp--0----------YadorigiSignalingConnector--------------------------------------groupNameHash:${groupNameHash}`);
-		const data = { command: 'get', group: groupNameHash, fileName };
-		this.l.log(`--getSdp--1----------YadorigiSignalingConnector--------------------------------------fileName:${fileName}`);
-		const result = await this.Fetcher.getTextCors(this.endPoint, data);
-		this.l.warn(`--getSdp--2----------YadorigiSignalingConnector--------------------------------------result:${result}`);
+		this.l.log(`--getSdp--1----------YadorigiSignalingConnector--GET-SDP-START-----------------------groupNameHash:${groupNameHash}/fileName:${fileName}`);
+		const result = await this.Fetcher.getTextCors(this.endPoint, { command: 'get', group: groupNameHash, fileName });
+		this.l.warn(`--getSdp--2----------YadorigiSignalingConnector--GET-SDP-DONE-----------------------result:${result}`);
 		return result;
 	}
 	async getLastOne(groupNameHash) {
-		this.l.log(`--getLastOne--0----------YadorigiSignalingConnector--------------------------------------groupNameHash:${groupNameHash}`);
-		const data = { command: 'last', group: groupNameHash };
-		this.l.log(`--getLastOne--1----------YadorigiSignalingConnector--------------------------------------this.endPoint:${this.endPoint}`);
-		const result = await this.Fetcher.getTextCors(this.endPoint, data);
-		this.l.warn(`--getLastOne--2----------YadorigiSignalingConnector--------------------------------------result:${result}`);
+		this.l.log(`--getLastOne--1----------YadorigiSignalingConnector--GET-LAST-ONE-START--------------groupNameHash:${groupNameHash}/this.endPoint:${this.endPoint}`);
+		const result = await this.Fetcher.getTextCors(this.endPoint, { command: 'last', group: groupNameHash });
+		this.l.warn(`--getLastOne--2----------YadorigiSignalingConnector--GET-LAST-ONE-DONE--------------result:${result}`);
 		return result;
 	}
 	async getNextOne(groupNameHash, fileName) {
-		this.l.log(`--getNextOne--0----------YadorigiSignalingConnector--------------------------------------groupNameHash:${groupNameHash}`);
-		const data = { command: 'last', group: groupNameHash, fileName };
-		this.l.log(`--getNextOne--1----------YadorigiSignalingConnector--------------------------------------fileName:${fileName}`);
-		const result = await this.Fetcher.getTextCors(this.endPoint, data);
-		this.l.warn(`--getNextOne--2----------YadorigiSignalingConnector--------------------------------------result:${result}`);
+		this.l.log(`--getNextOne--0----------YadorigiSignalingConnector--GET-NEXT-ONE-START--------------groupNameHash:${groupNameHash}/fileName:${fileName}`);
+		const result = await this.Fetcher.getTextCors(this.endPoint, { command: 'last', group: groupNameHash, fileName });
+		this.l.warn(`--getNextOne--2----------YadorigiSignalingConnector--GET-NEXT-ONE-DONE--------------result:${result}`);
 		return result;
 	}
 	async getSdpByName(fileName) {
