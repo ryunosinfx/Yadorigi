fd9c6b7a244cb26f3a8c2fd630d5670af8b912e9
ryunosinfx <ryunosinfxxul@gmail.com>
2022-10-11 01:49:53 +0900

    再度GASテスト

--- src/util/Fetcher.js
+++ src/util/Fetcher.js
@@ -35,6 +35,7 @@ export class Fetcher {
 			requestData.referrer = 'no-referrer';
 		} else if (isPost) {
 			requestData['Content-Type'] = 'application/x-www-form-urlencoded';
+			requestData.mode = 'no-cors';
 		}
 		const isObj = typeof data === 'object';
 		if (isPost) {
